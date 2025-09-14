7aea2e4698e6f546aee6e4110072b265d7805d16
Lucas Silva <lucasandrade730@gmail.com>
2025-09-14 17:28:41 -0300

    feat: implement Sidebar and TopBar components with layout structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,11 @@
-import { LinkButton } from '@/components/LinkButton';
-import Image from 'next/image';
+import { Sidebar } from '@/components/Sidebar';
+import TopBar from '@/components/TopBar';
 
 export default function Home() {
   return (
-    <aside className='sidebar inline-flex flex-col p-2 gap-3 rounded-xl items-center'>
-      {/* Logo */}
-      <Image src='/Logo.svg' alt='Logo' width={92} height={92} />
-
-      {/* Divider */}
-      <div className='sidebar-separator h-[2px] w-[160px] blur-[2px]' />
-
-      {/* Buttons */}
-      <LinkButton label='Página Inicial' icon='home' href='/' />
-      <LinkButton label='Programas' icon='article' href='/' />
-      <LinkButton label='Relatório' icon='analytics' href='/' disabled />
-      <LinkButton label='Configuração' icon='settings' href='/' />
-      <LinkButton label='Informação' icon='info' href='/' disabled />
-
-      {/* Divider */}
-      <div className='sidebar-separator h-[2px] w-[160px] blur-[2px]' />
-
-      {/* Button */}
-      <LinkButton label='Alterar Tema' icon='dark_mode' href='/' disabled />
-    </aside>
+    <>
+      <TopBar />
+      <Sidebar />
+    </>
   );
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,12 +1,27 @@
+import Image from 'next/image';
+import { LinkButton } from '../LinkButton';
+
 export function Sidebar() {
   return (
-    <>
-      <h2>Sidebar</h2>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-      </ul>
-    </>
+    <aside className='sidebar inline-flex flex-col p-2 gap-3 rounded-xl items-center'>
+      {/* Logo */}
+      <Image src='/Logo.svg' alt='Logo' width={92} height={92} />
+
+      {/* Divider */}
+      <div className='sidebar-separator h-[2px] w-[160px] blur-[2px]' />
+
+      {/* Buttons */}
+      <LinkButton label='Página Inicial' icon='home' href='/' />
+      <LinkButton label='Programas' icon='article' href='/' />
+      <LinkButton label='Relatório' icon='analytics' href='/' />
+      <LinkButton label='Configuração' icon='settings' href='/' />
+      <LinkButton label='Informação' icon='info' href='/' />
+
+      {/* Divider */}
+      <div className='sidebar-separator h-[2px] w-[160px] blur-[2px]' />
+
+      {/* Button */}
+      <LinkButton label='Alterar Tema' icon='dark_mode' href='/' />
+    </aside>
   );
 }
