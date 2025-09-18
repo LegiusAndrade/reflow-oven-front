b5e336aa501874f4fb05319fbc478910a7c52780
Lucas Silva <lucas.silva@jassy.ag>
2025-09-18 13:53:34 -0300

    [Wip] refactor: simplify LinkButton structure and update Sidebar layout styles

--- src/components/LinkButton/index.tsx
+++ src/components/LinkButton/index.tsx
@@ -15,33 +15,30 @@ export function LinkButton({ label, icon, href, iconSizePx = 24, fill = 1, weigh
   //sm(640), md(768), lg(1024), xl(1280), 2xl(1536)
 
   return (
-    <div>
-      <Link
-        href={href}
-        className={clsx(
-          'inline-flex', // Display as an inline-level flex container (stays inline with text/siblings)
-          'items-center', // Center items on the cross axis (vertical by default in a row layout)
-          'justify-start', // Align items to the start of the main axis (left by default in a row layout)
-          'select-none', // Prevent text selection (improves button UX)
-          'sm:px-20 sm:py-1', // Small padding on small screens and up
-          'lg:px-20 lg:py-1  ', // Medium padding on medium screens and up
-          'xl:px-20 xl:py-3', // Larger padding on large screens and up
-          disabled ? 'btn-link-disabled cursor-not-allowed pointer-events-none' : 'btn-link', // Apply disabled styles if disabled, otherwise apply normal button styles
-          'border-8 border-solid',
-          'border-amber-200'
-        )}
+    <Link
+      href={href}
+      className={clsx(
+        'inline-flex', // Display as an inline-level flex container (stays inline with text/siblings)
+        'items-center', // Center items on the cross axis (vertical by default in a row layout)
+        'justify-start', // Align items to the start of the main axis (left by default in a row layout)
+        'select-none', // Prevent text selection (improves button UX)
+        disabled ? 'btn-link-disabled cursor-not-allowed pointer-events-none' : 'btn-link', // Apply disabled styles if disabled, otherwise apply normal button styles
+        'px-6 py-4 text-md ', // Largest padding on extra large screens and up
+        'sm:px-2 sm:py-2', // Small padding on small screens and up
+        'lg:px-2 lg:py-1', // Medium padding on medium screens and up
+        'xl:px-2 xl:py-3' // Larger padding on large screens and up
+      )}
+    >
+      <span
+        className={clsx('material-symbols-rounded')}
+        style={{
+          fontSize: `${iconSizePx}px`,
+          fontVariationSettings: `'FILL' ${fill}, 'wght' ${weight}, 'GRAD' 0, 'opsz' ${iconSizePx}`,
+        }}
       >
-        <span
-          className={clsx('material-symbols-rounded')}
-          style={{
-            fontSize: `${iconSizePx}px`,
-            fontVariationSettings: `'FILL' ${fill}, 'wght' ${weight}, 'GRAD' 0, 'opsz' ${iconSizePx}`,
-          }}
-        >
-          {icon}
-        </span>
-        <span className='ml-2'>{label}</span>
-      </Link>
-    </div>
+        {icon}
+      </span>
+      <span className='ml-2'>{label}</span>
+    </Link>
   );
 }
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,26 +1,27 @@
 import { clsx } from 'clsx';
 import Image from 'next/image';
+import { LinkButton } from '../LinkButton';
 
 export function Sidebar() {
   return (
-    <aside className={clsx('sidebar', 'flex', 'inline-flex', 'border-8', 'border-cyan-200', 'p-2', 'gap-3', 'rounded-xl', 'items-center')}>
+    <aside className={clsx('sidebar', 'inline-flex', 'flex-col', 'border-8', 'border-cyan-200', 'p-2', 'gap-3', 'rounded-xl', 'items-center')}>
       {/* Logo */}
       <Image src='/Logo.svg' alt='Logo' width={92} height={92} priority />
 
       {/* Divider */}
-      <div className='sidebar-separator h-[4px] w-[calc(100%_-_4px)] mx-[2px] my-4' />
+      <div className='sidebar-separator w-full blur-[2px] h-[2px] rounded-full' />
 
       {/* Buttons */}
-      {/* <section className='flex flex-col w-full'>
+      <section className='inline-flex flex-col w-full gap-2'>
         <LinkButton label='Página Inicial' icon='home' href='/' />
         <LinkButton label='Programas' icon='article' href='/' />
         <LinkButton label='Relatório' icon='analytics' href='/' />
         <LinkButton label='Configuração' icon='settings' href='/' />
         <LinkButton label='Informação' icon='info' href='/' />
-      </section> */}
+      </section>
 
       {/* Divider */}
-      {/* <div className='sidebar-separator h-[2px] w-[160px] blur-[2px]' /> */}
+      <div className='sidebar-separator w-full blur-[2px] h-[2px] rounded-full' />
 
       {/* Button */}
       {/* <LinkButton label='Alterar Tema' icon='dark_mode' href='/' /> */}
